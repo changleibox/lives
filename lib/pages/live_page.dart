419cba876246a0d21ba7a459c1858313f566333b
changlei <changlei@graspyun.com>
2022-01-28 18:00:16 +0800

    feat: 初始提交

--- lib/pages/live_page.dart
+++ lib/pages/live_page.dart
@@ -134,11 +134,11 @@ class _LivePresenter extends VoidPresenter<LivePage> {
 
   Future<void> _startLive() async {
     try {
+      await _model.liveType.start();
       await FutureWrapper.wrapLoading(
         context: context,
         computation: _model.startLive,
       );
-      await _model.liveType.start();
     } on LiveError catch (e) {
       showToast(e.message.toString());
     }
@@ -146,11 +146,11 @@ class _LivePresenter extends VoidPresenter<LivePage> {
 
   Future<void> _exitLive() async {
     try {
-      await _model.liveType.exit();
       await FutureWrapper.wrapLoading(
         context: context,
         computation: _model.exitLive,
       );
+      await _model.liveType.exit();
     } on LiveError catch (e) {
       showToast(e.message);
     }
